ea38967cf2784dd59ed95de8b485fe84f0ca4b26
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-11-04 12:56:41 +0000

    Adding the drop down brand menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+    const[state, setState] = useState("")
+  
+  
+  
   return (
-    <div className="App">
-      <h1>React Here</h1>
+    <div className="container p-5">
+        <select className="custom-select">
+                <option value="Nike">Nike</option>
+        <option value="Jordan">Jordan</option>
+        <option value="Converse">Converse</option>
+        <option value="Adidas">Adidas</option>
+        <option value="Vans">Vans</option>
+        <option value="Puma">Puma</option>
+        <option value="Reebok">Reebok</option>
+        <option value="NewBalance">New Balance</option>
+        <option value="Saucony">Saucony</option>
+        <option value="Asics">Asics</option>
+        <option value="Under Armour">Under Armour</option>
+        </select>
     </div>
-  );
+  )
 }
 
 export default App;
