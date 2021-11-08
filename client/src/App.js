5c2ba9473cdcc77a6998623728ed1e2a6e4a7cc1
Ifrah Hussein <Ifrahhssn@outlook.com>
2021-11-08 15:57:39 +0000

    added styling to navbar css

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,48 @@
 import React , { Component, Fragment }  from 'react';
 // import { BrowserRouter as Router, Routes, Link } from "react-router-dom";
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+
 import './App.css';
-// import Options from './components/Options';
+
+
 
 export default function App() {
   return (
   
-   <BrowserRouter>
-    <main>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/search">Search</Link></li>
-          <li><Link to="/quiz">Quiz</Link></li>
-        </ul>
-      </nav>
+  <BrowserRouter>
+
+  <body>
+    <div>
+    <nav>
+      <div class="container-fluid">
+        <a class="navbar-brand">Sneaker Seeker</a>
+        <div>
+          <ul>
+            <li><Link to="/search">Search</Link></li>
+            <li><Link to="/quiz">Quiz</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div> 
     <Routes>
     <Route path="/" exact component={Home} />
     <Route path="/search"  component={Search} />
     <Route path="/quiz"  component={Quiz} />
     </Routes>
 
-    <div>
-      <h1>Find your perfect Sneaker</h1>
-      <h3>Use the search engine to generate matches</h3>
+    <div class="page-container">
+      <h1 class="tag-line">Find your perfect Sneaker</h1>
+      <h3 id="tag">Use the search engine to generate matches</h3>
+      </div>
+      <div>
+    <button class="btn" type="button">Search</button> 
     </div>
-    <button type="button">Search</button> 
-    <button type="button">Quiz</button> 
-
-    </main>
-</BrowserRouter>
+    <div>
+    <button class="btn" type="button">Quiz</button> 
+    </div>
+  </body>
+  </BrowserRouter>
 
   );
 }
