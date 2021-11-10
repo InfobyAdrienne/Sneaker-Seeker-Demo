d75a917a78e903d87ed8a62078c3b0ef37c308cf
Leah <leahpitter95@gmail.com>
2021-11-10 16:28:34 +0000

    empty cards for search page, but currently saved in results.js

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,9 +1,43 @@
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+// const Results = (props) => {
+//   return (
+//     <h1>results page</h1>
+//   )
+// }
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Row, Col, Card, CardBody, CardTitle, CardText, CardImg
+} from 'reactstrap'
+import './index.css'
+import './Search.js'
 
+    const Results = (sneaker) => {
+        return (
+          <div>
+            <div>
+               <Row className="App">
+                <Col xs="6">
+                  <Card>
+                    {/* <CardImg top width="100%" src={(sneaker.media.smallImageUrl)}></CardImg> */}
+                    <CardBody>
+                      <CardTitle>
+                        {sneaker.id}
+                      </CardTitle>
+                      <CardText>
+                        {sneaker.retailPrice}
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        )
+      }
 
-const Results = (props) => {
-  return (
-    <h1>results page</h1>
-  )
-}
-export default Results;
+export default Results
